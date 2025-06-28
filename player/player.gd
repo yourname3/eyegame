e57bedcda98d7c891ee5ba81e81f3618bccafa37
bwoodward523 <bwoodward@ycp.edu>
2025-06-28 12:11:24 -0400

    Player Movement, More floaty but still responsive

--- player/player.gd
+++ player/player.gd
@@ -1,26 +1,27 @@
 extends CharacterBody2D
 class_name Player
 
-@export var speed = 400
+@export var speed := 700
+@export var brake_force := 0.94
+@export var acceleration := 15.0
 
-# TEMPORARY PLAYER CODE FOR ANIMATION
-func _process(delta: float) -> void:
-	
-	# old movement code (doesnt work)
-	#var target = get_global_mouse_position()
-	#var target_vel = (target - position).normalized() * 300
-	#if target.distance_to(position) < 100:
-		#target_vel = Vector2.ZERO
-	#
-	#velocity = velocity.move_toward(target_vel, 500 * delta)
-	#move_and_slide()
-	pass
-	
-func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_direction * speed
 
-func _physics_process(delta):
-	get_input()
+func get_input() -> Vector2:
+	return Input.get_vector("Left", "Right", "Up", "Down")
+
+func _physics_process(delta: float) -> void:
+	var dir := get_input().normalized()
+	var target_velocity := dir * speed
+
+	# Apply acceleration if there's input
+	if dir != Vector2.ZERO:
+		# Accelerate toward target velocity
+		velocity = velocity.lerp(target_velocity, acceleration * delta)
+		#print("speed = ", velocity.length())
+	else:
+		# Apply braking force (simulates deceleration)
+		velocity *= brake_force
+	
+
 	move_and_slide()
-	
+
