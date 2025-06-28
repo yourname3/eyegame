0ea83ff5d16f2df87b65367ed85667f2cba32d7a
ben <bwall4@gmu.edu>
2025-06-27 23:34:49 -0400

    One option for global-coordinate based animation

--- player/player_animation.gd
+++ player/player_animation.gd
@@ -2,15 +2,15 @@ extends Node2D
 class_name PlayerAnimation
 
 var _last_position := Vector2.ZERO
+var _position_delta := Vector2.ZERO
 
 func _ready() -> void:
 	_last_position = get_parent().position
 	
 func _physics_process(delta: float) -> void:
 	var current_position: Vector2 = get_parent().position
+	_position_delta = current_position - _last_position
 	var parent_vel := (current_position - _last_position) / delta
-	
-	print(parent_vel)
 	
 	if parent_vel.length_squared() > 20:
 		var target_angle := parent_vel.angle()
--- player/player_flagellum.gd
+++ player/player_flagellum.gd
@@ -3,6 +3,10 @@ class_name PlayerFlagellum
 
 const POINT_COUNT = 32
 
+var global_points := PackedVector2Array()
+var source_points := PackedVector2Array()
+var offsets       := PackedVector2Array()
+
 func _ready() -> void:
 	# We must have 2 points, the second one determines our size.
 	assert(points.size() >= 2)
@@ -13,6 +17,9 @@ func _ready() -> void:
 	
 	for i in range(0, POINT_COUNT):
 		add_point(Vector2(step * i, 0))
+		source_points.append(Vector2(step * i, 0))
+		global_points.append(global_transform * Vector2(step * i, 0))
+		offsets.append(Vector2.ZERO)
 		
 	_phase = position.y * 3
 
@@ -21,5 +28,19 @@ var _phase_rate := randf_range(4.5, 5.5)
 
 func _process(delta: float) -> void:
 	_phase = fmod(_phase + delta * _phase_rate, TAU)
+	var inv = global_transform.inverse()
+	
+	# Update offsets
+	for i in range(points.size() - 1, 0, -1):
+		offsets[i] = offsets[i - 1]
+	offsets[0] = get_parent()._position_delta
+	
+	# Compute following thing
 	for i in range(0, points.size()):
-		points[i].y = sin(i * 0.3 + _phase) * i
+		global_points[i] = global_transform * (source_points[i] + offsets[i])
+		
+	
+	
+	for i in range(0, points.size()):
+		points[i] = inv * global_points[i]
+		#points[i].y = sin(i * 0.3 + _phase) * i
