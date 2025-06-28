242a2ad7cd1231cec17c0e218a54bc20d64d456f
Pedro Burset <bursetpr@gmail.com>
2025-06-28 11:19:26 -0400

    psudocode for a game director/wave spawener object, fixed player movement

--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,26 @@
 extends CharacterBody2D
 class_name Player
 
+@export var speed = 400
+
 # TEMPORARY PLAYER CODE FOR ANIMATION
 func _process(delta: float) -> void:
-	var target = get_global_mouse_position()
-	var target_vel = (target - position).normalized() * 300
-	if target.distance_to(position) < 100:
-		target_vel = Vector2.ZERO
 	
-	velocity = velocity.move_toward(target_vel, 500 * delta)
+	# old movement code (doesnt work)
+	#var target = get_global_mouse_position()
+	#var target_vel = (target - position).normalized() * 300
+	#if target.distance_to(position) < 100:
+		#target_vel = Vector2.ZERO
+	#
+	#velocity = velocity.move_toward(target_vel, 500 * delta)
+	#move_and_slide()
+	pass
 	
+func get_input():
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
 	move_and_slide()
+	
