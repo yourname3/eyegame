d10c5dca6afa6d48c89d0a6a191a5fbcef904c5b
magister willowstar <legionheir90@gmail.com>
2025-06-29 08:33:58 -0700

    update enemy

--- enemies/basic_enemy/scripts/cataracts/sensory.gd
+++ enemies/basic_enemy/scripts/cataracts/sensory.gd
@@ -15,8 +15,8 @@ func _process(delta: float) -> void:
 		Globals.Status.RUNNING:
 			agent.set_target_position(target.global_position)
 			signal_bus.look_at(agent.target_position)
-			signal_bus.use_skill.emit()
 			signal_bus.use_engage.emit(agent.get_next_path_position())
+			signal_bus.use_aggression.emit()
 		Globals.Status.FAILURE:
 			agent.set_target_position(target.global_position)
 			signal_bus.look_at(agent.target_position)
