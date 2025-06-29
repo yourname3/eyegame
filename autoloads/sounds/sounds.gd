38c2c6a7ff7804058efec11a44409c586c2a1912
bwoodward523 <bwoodward@ycp.edu>
2025-06-29 17:30:52 -0400

    Sounds & UI

--- autoloads/sounds/sounds.gd
+++ autoloads/sounds/sounds.gd
@@ -23,6 +23,7 @@ var current_music := -1
 @onready var sfx_item_select := $ItemSelect
 @onready var sfx_menu_hover := $MenuHover
 @onready var sfx_player_hit := $PlayerHit
+@onready var sfx_eye_hit := $EyeHit
 @onready var sfx_shotgun := $Shotgun
 
 func play_music(idx: int) -> void:
