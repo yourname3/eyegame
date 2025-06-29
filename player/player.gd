205fa523e35f13048e462ee470f3ef4db1ec9fed
ben <bwall4@gmu.edu>
2025-06-29 15:46:10 -0400

    Basic waves

--- player/player.gd
+++ player/player.gd
@@ -68,7 +68,7 @@ func _on_exp_collect_body_entered(body: Node2D) -> void:
 		
 func _compute_needed_exp():
 	var x = (level + 1) # experience to the next level, e.g. when level = 0 we get x = 1
-	needed_exp = int(10 * pow(1.1, x))
+	needed_exp = int(20 * pow(1.1, x))
 
 func check_level_up():
 	if current_exp >= needed_exp:
