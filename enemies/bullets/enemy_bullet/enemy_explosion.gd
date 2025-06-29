b4636ee19b0dc0cfa958f275d9f78c79b9977b0f
magister willowstar <legionheir90@gmail.com>
2025-06-29 06:20:23 -0700

    enemy projectiles should now work

---
+++ enemies/bullets/enemy_bullet/enemy_explosion.gd
@@ -0,0 +1,17 @@
+extends Node2D
+class_name EnemyExplosion
+
+#func _ready():
+	#$LifeTime.start(.8)
+	
+
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Explode":
+		queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Players"):
+		Globals.transmit_damage.emit(body, 3)
