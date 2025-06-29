1b0a6180b5a03d31c0c4b0085a02ee20c34ce358
magister willowstar <legionheir90@gmail.com>
2025-06-28 22:37:42 -0700

    enemies now move...long range distance still needs to be tested

---
+++ enemies/bullets/enemy_bullet/bullet.gd
@@ -0,0 +1,10 @@
+extends BasicBullet
+class_name EnemyBullet
+
+
+
+
+
+func _ready() -> void:
+	speed = 300.0
+	damage = 1.0
---
+++ enemies/bullets/enemy_bullet/explosive_bullet.gd
@@ -0,0 +1,24 @@
+extends BasicBullet
+class_name ExplosiveBullet
+
+
+
+# the explosion
+
+
+
+
+
+func _ready() -> void:
+	speed = 280.0
+	damage = 5.0
+
+
+
+
+
+func _on_life_time_timeout():
+	var new_explo = Globals.explosion.instantiate()
+	new_explo.global_position = global_position
+	add_sibling(new_explo)
+	queue_free()
