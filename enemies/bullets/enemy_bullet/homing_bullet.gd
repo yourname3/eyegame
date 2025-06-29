a7201f6a98ce721f2c8ee26c61c94eaf21495f0b
magister willowstar <legionheir90@gmail.com>
2025-06-29 09:04:41 -0700

    enemies have weapons

--- enemies/bullets/enemy_bullet/homing_bullet.gd
+++ enemies/bullets/enemy_bullet/homing_bullet.gd
@@ -5,12 +5,15 @@ class_name HomingBullet
 
 
 @export var agent : NavigationAgent2D 
-
-
+var target : Player 
 
 func _ready() -> void:
+	print(target)
 	speed = 350
 	damage = 3.0
+	agent.set_target_position(target.global_position)
 
 func _physics_process(delta: float) -> void:
 	position += position.direction_to(agent.get_next_path_position()) * speed * delta
+	agent.set_target_position(target.global_position)
+	
