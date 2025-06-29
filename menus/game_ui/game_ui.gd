9e13cd12b4c20d632659bf15189673ae37f1cfd4
bwoodward523 <bwoodward@ycp.edu>
2025-06-29 14:53:22 -0400

    Game UI

--- menus/game_ui/game_ui.gd
+++ menus/game_ui/game_ui.gd
@@ -9,24 +9,20 @@ var right_idx := 0
 var panels_right = []
 
 func _ready():
-	for i in range(3):
-		var gp = gun_panel.instantiate()
-		panels_left.append(gp)
-		gp.weapon_idx = i
-		vbox_left.add_child(gp)
-	for i in range(3):
-		var gp = gun_panel.instantiate()
-		panels_right.append(gp)
-		gp.weapon_idx = i
-		vbox_right.add_child(gp)
+	create_ui()
 	
 func switch_panel(is_left):
 	if is_left:
+		panels_left[left_idx].selected = false
 		left_idx = (left_idx + 1) % panels_left.size() 
 		print("ui_left_idx: ", left_idx)
+		panels_left[left_idx].selected = true
 	if !is_left:
+		panels_right[right_idx].selected = false
 		right_idx = (right_idx + 1) % panels_right.size() 
 		print("ui_right_idx: ", right_idx)
+		panels_right[right_idx].selected = true
+	
 		
 func update_ammo_text():
 	for i in panels_left:
@@ -34,4 +30,22 @@ func update_ammo_text():
 	for i in panels_right:
 		i.set_ammo_text()
 	
-	
+func create_ui():
+	#Get owned weapons
+	var owned_weapon_count = Globals.OWNED_WEAPONS.size()
+	for i in range(owned_weapon_count):
+		var gp = gun_panel.instantiate()
+		panels_left.append(gp)
+		gp.weapon_idx = i
+		if i == 0:
+			gp.selected = true
+		vbox_left.add_child(gp)
+	for i in range(owned_weapon_count):
+		var gp = gun_panel.instantiate()
+		panels_right.append(gp)
+		gp.weapon_idx = i
+		if i == 0:
+			gp.selected = true
+		gp.layout_direction = 3
+		gp.is_right = true
+		vbox_right.add_child(gp)
--- menus/game_ui/gun_window.gd
+++ menus/game_ui/gun_window.gd
@@ -1,6 +1,21 @@
-extends Panel
+extends Control
 
 var weapon_idx = 0
-
+var selected = false
+var selected_size = Vector2(1.5,1)
+var default_size = Vector2(1,1)
+var is_right = false
 func set_ammo_text():
-	$Label3.text = str(Globals.CURRENT_AMMO[weapon_idx] , " / " , Globals.MAX_AMMO[weapon_idx])
+	if weapon_idx != 0:
+		$AspectRatioContainer/Panel/Label3.text = str(Globals.CURRENT_AMMO[weapon_idx] , " / " , Globals.MAX_AMMO[weapon_idx])
+	else:
+		$AspectRatioContainer/Panel/Label3.text = "∞ / ∞"
+	if selected:
+		$AspectRatioContainer/Panel.size = selected_size * 100
+		if is_right:
+			position.x = -50
+	else:
+		$AspectRatioContainer/Panel.size = default_size * 100
+		if is_right:
+			position.x = 0
+	
